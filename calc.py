f4df3eb7d04b0e29b6cdbd808788279db4e47184
Elena Zaharkina <zaharkinaen@mail.ru>
2023-01-25 23:00:52 +0700

    initial

---
+++ calc.py
@@ -0,0 +1,14 @@
+﻿while True:
+    num1 = int(input('Введите 1 число: '))
+    num2 = int(input('Введите 2 число: '))
+    do = input('Введите действие: ')
+    if do == '+':
+        print(num1, '+', num2, '=', num1 + num2)
+    elif do == '-':
+        print(num1, '-', num2, '=', num1 - num2)
+    elif do == '*':
+        print(num1, '*', num2, '=', num1 * num2)
+    elif do == '/':
+        print(num1, '/', num2, '=', num1 / num2)
+    else:
+        print('Ошибка: такой операции не существует. Попробуйте ещё раз.')
