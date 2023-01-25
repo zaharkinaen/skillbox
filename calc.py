cf71c5479b7baf4930cc86c7a768c608e472c3be
Elena Zaharkina <zaharkinaen@mail.ru>
2023-01-26 00:22:41 +0700

    changed calc.py

--- calc.py
+++ calc.py
@@ -1,6 +1,6 @@
 ﻿while True:
-    num1 = int(input('Введите первое число: '))
-    num2 = int(input('Введите второе число: '))
+    num1 = int(input('Введите 1 число: '))
+    num2 = int(input('Введите 2 число: '))
     do = input('Введите действие: ')
     if do == '+':
         print(num1, '+', num2, '=', num1 + num2)
