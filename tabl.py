332b9fcd5f75e78284ab0fc99790001ed505e40e
Elena Zaharkina <zaharkinaen@mail.ru>
2023-01-26 17:04:48 +0700

    table changes

--- tabl.py
+++ tabl.py
@@ -2,7 +2,7 @@
 print('Таблица умножения')
 print('----------------------')
 
-for i in range(1, 10):
+for i in range(1, 8):
     for j in range(1, 10):
         print(i, '*', j, '=', i * j, end = '\n')
     print()
